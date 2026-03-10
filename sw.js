a80c025d7af44af61b6756ecb51af353ed9a1e97
ckempadoo <ckempadoo@directmaildepot.com>
2026-03-10 10:58:36 -0400

    Update sw.js
    Signed-off-by: ckempadoo <ckempadoo@directmaildepot.com>

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-self.addEventListener('install',e=>{
-e.waitUntil(caches.open('roll-cache').then(c=>c.addAll(['./','./index.html'])));
+self.addEventListener('install', e => {
+self.skipWaiting();
 });
-self.addEventListener('fetch',e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener('fetch', e => {
+e.respondWith(fetch(e.request));
 });
